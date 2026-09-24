737895dd905793c2f62281f6234a57ac294b3d21
ronivancito-ux <ronivancito@gmail.com>
2026-09-23 21:06:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Ronald Nuñez")
+
+
+archivo = st.file_uploader("Cargue su archivo")
+
+if archivo is not None:
+  tabla = pd.read_csv(archivo)
+  st.write(tabla)
